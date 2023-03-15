131d3e5d40fa5fbeb503cdf8cc7a1d1a9d0c6a39
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-03-15 16:49:37 -0500

    removed if statment, causing error bc file already exists:
    Removed because that the file was alrady existing so that the file writes

--- corperation/.app/apps/meetingOrganizer.lua
+++ corperation/.app/apps/meetingOrganizer.lua
@@ -41,11 +41,53 @@ local function writeMeetingDataToFile()
     file:write("\n")
 
     file:close()
+    print("Meeting created! Check the explorer for openMeetings.txt")
+    
+end
+writeMeetingDataToFile()local meetingName, meetingURL, meetingTimes, meetingOrganizer, hostContactEmail, otherNotes
 
-    if file == true then
-        print("Meeting created! Check the explorer for openMeetings.txt")
-    else
-        print("ERROR: meeting file not created. To solve this issue please rerun this file or try again later.")
-    end
+local function organizerInformation()
+
+    io.write("Meeting Name: ")
+    meetingName = io.read()
+
+    io.write("Meeting App Host: ")
+    local meetingAppHost = io.read()
+
+    io.write("Meeting URL: ")
+    meetingURL = io.read()
+
+    io.write("Meeting Times: ")
+    meetingTimes = io.read()
+
+    io.write("Meeting Organizer: ")
+    meetingOrganizer = io.read()
+
+    io.write("Host contact email: ")
+    hostContactEmail = io.read()
+
+    io.write("Other Notes (optional): ")
+    otherNotes = io.read()
+
+end
+
+organizerInformation()
+
+local function writeMeetingDataToFile()
+
+    local file = io.open("openMeetings.txt", "a")
+
+    file:write("\n----- NEW MEETING -----\n")
+    file:write("Meeting Name: " .. meetingName .. "\n")
+    file:write("Meeting URL: " .. meetingURL .. "\n")
+    file:write("Meeting times: " .. meetingTimes .. "\n")
+    file:write("Organizer: " .. meetingOrganizer .. "\n")
+    file:write("Host contact email: " .. hostContactEmail .. "\n")
+    file:write("Other notes: " .. otherNotes .. "\n")
+    file:write("\n")
+
+    file:close()
+    print("Meeting created! Check the explorer for openMeetings.txt")
+    
 end
 writeMeetingDataToFile()
